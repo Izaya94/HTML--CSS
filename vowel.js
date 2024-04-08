50ba24166d214718224cd65ab0a0f293478e86a6
Izaya94 <pujarakoral@gmail.com>
2024-04-08 11:56:33 +0530

    08-04

--- vowel.js
+++ vowel.js
@@ -1,18 +1,28 @@
+function checkforvowel() {
+  userinput = document.getElementById("vowelorno").value;
+  useroutput = document.getElementById("answerifvowelorno");
 
-function checkforvowel(){
-    userinput = document.getElementById("vowelorno").value;
-    useroutput = document.getElementById("answerifvowelorno");
+  userinputnew = userinput.toLowerCase();
 
-    userinputnew = userinput.toLowerCase();
+  console.log(userinputnew);
 
-    console.log(userinputnew);
+  let letters = /^[a-z]+$/;
 
-    if (userinputnew == 'a' || userinputnew == 'e' || userinputnew == 'i' || userinputnew == 'o' || userinputnew == 'u')
-    {
-        useroutput.value = ("The given value is a vowel");
+  if (userinputnew.match(letters)) {
+    if (
+      userinputnew == "a" ||
+      userinputnew == "e" ||
+      userinputnew == "i" ||
+      userinputnew == "o" ||
+      userinputnew == "u"
+    ) {
+      useroutput.value = "The given value is a vowel";
+    } else {
+      useroutput.value = "The given value is a consonant";
     }
-    else 
-    {
-        useroutput.value = ("The given value is a consonant");
-    }
+  }
+  else
+  {
+    useroutput.value = "Enter a alphabet to get proper response.";
+  }
 }
